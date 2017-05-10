1ece661f96e6de3fe0dc95f6b3a57f3be5108ee9
xwatson <wusheng.xu@oborcloud.com>
2017-05-10 11:45:44 +0800

    添加页面权限验证

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -2,10 +2,14 @@
  * Created by xwatson on 2016/12/9.
  */
 import React, { Component, PropTypes } from 'react'
+import { message } from 'antd'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 import LoadingBar from '../components/LoadingBar'
+import Auth from '../utils/Auth'
 const config = require('../../config/config.json')
+const reg = /^\/(\w+)\/?/
+
 class AppContainer extends Component {
     static propTypes = {
         routes : PropTypes.any,
@@ -15,7 +19,6 @@ class AppContainer extends Component {
     static host = {
         BASE_API_URL:config[NODE_ENV.toUpperCase()].apiHost
     }
-
     shouldComponentUpdate() {
         return false
     }
@@ -40,6 +43,25 @@ class AppContainer extends Component {
 }
 
 function requireLogin(nextState, replace) {
-    // console.log("requireLogin(nextState, replace)", nextState, replace)
+    if (nextState.location.pathname !== '/') {
+        const meuns = (Auth.getAccount() || {}).Auth.AuthMenu || []
+        let authRouter = false
+        let match = nextState.location.pathname.match(reg)
+        match = (match && match[0]) ? match[0] : ''
+        if (match.lastIndexOf('/') > 0) {
+            match = match.substr(0, match.length - 1)
+        }
+        meuns.forEach((item) => {
+            if (match === item.router && !authRouter) {
+                authRouter = true
+            }
+        })
+        if (!authRouter) {
+            message.error('您没有权限访问此页面！', 3)
+            replace({
+                pathname: '/'
+            })
+        }
+    }
 }
 export default AppContainer
